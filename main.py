9d6124a077924e418ce070ad1d6bf005df61bc47
Murkyshadow <aleks6553914@mail.ru>
2022-07-22 17:06:28 +0300

    вынес настройки в отдельную функцию, переделал прыжок и переписал код движения, плавное обновление карты при новой высоте

--- main.py
+++ main.py
@@ -9,6 +9,176 @@ import pygame
 
 class tetge():
     def __init__(self):
+        self.max_h = 0  # значение максимальной достигнутой высоты персонажем
+        self.field = []  # поле 24 на 18
+        self.now_blocks = []  # блоки в падении
+        self.now_animation = False  # обозначает, что сейчас не происходит прорисовка падения блоков
+        self.isjump = False
+
+        pygame.font.init()
+        self.setting()
+
+        for i in range(self.field_size[0]//self.size_block):
+            self.field.append([0] * (self.field_size[1]//self.size_block+4))  # одновременно видно будет только 24 клетки (вверх) и 18 клеток в ширину
+        self.height = [0] * (self.field_size[0]//self.size_block)  # высота каждого столбца
+
+        # jump:
+        self.isjump = False   # сейчас игрок не в прыжке
+        self.isfall = False
+        self.jump_speed = 0
+        # height_jump = 55
+        # max_jump = 55
+        # jumpCount = 10
+
+        while 1:
+            # col_pl = self.coor_player[0] // 24  # X персонажа
+            # row_pl = len(self.field[0]) - self.coor_player[1] // 24 - 5  # Y персонажа
+            pygame.time.delay(self.speed_game)
+            self.score()
+
+            win.blit(self.player_img, self.coor_player)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    # if event.key == pygame.K_DOWN:
+                    #     self.max_h += 1
+                    #     print(self.max_h)
+                    if event.key == pygame.K_UP and not self.isjump and not self.isfall:  # прыжок
+                        self.isjump = True
+                        self.jump_speed = 2
+                        self.height_j = 0
+                        # height_jump = 0
+                        # jumpCount = 4
+
+                    if event.key == pygame.K_SPACE:  # ставим синий блок
+                        # win.blit(pygame.image.load('img/cube_blue.png'), (col_pl * 24, (23 - row_pl) * 24))
+                        # print(col_pl, row_pl)
+                        # pygame.display.update()
+                        print("высота сейчас", self.max_h)
+
+
+            self.move()
+
+            # if self.field[(self.coor_player[0] + 1) // 24][row_pl] != 0 or self.field[(self.coor_player[0] + 15) // 24][
+            #     row_pl] != 0:  # если достигает блока сверху, то сразу опускается вниз
+            #     height_jump = max_jump
+
+            # if self.now_jump  and self.field[col_pl][row_pl] == 0 and \
+            #         self.field[(self.coor_player[0] + size_pl[0] - 2) // 24][row_pl] == 0:  # взлет прыжка
+            #     print('jump')
+            #     win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], size_pl[0], size_pl[1]))
+            #     if jumpCount >= 0:
+            #         self.coor_player[1] -= int(jumpCount**2)//2
+            #         jumpCount -= 0.2
+            #     else:
+            #         jumpCount -= 0.2
+            #         self.now_jump = False
+            #
+            #     win.blit(self.player_img, self.coor_player)
+            #     pygame.display.update()
+            #     height_jump += (jumpCount**2)//2
+            #
+            # elif self.coor_player[1] + (jumpCount ** 2)//2 < 552 and (row_pl != 0 and self.field[col_pl][row_pl - 1] == 0 and self.field[(self.coor_player[0] + size_pl[0] - 2) // 24][row_pl - 1] == 0):  # падение
+            #     win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], size_pl[0], size_pl[1]))
+            #
+            #     self.coor_player[1] += int(jumpCount ** 2)//2
+            #     jumpCount -= 0.3
+            #
+            #     win.blit(self.player_img, self.coor_player)
+            #     pygame.display.update()
+            #
+            # elif self.coor_player[1] + (jumpCount ** 2) >= 552:
+            #     self.now_jump = False
+            #     win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], size_pl[0], size_pl[1]))
+            #     self.coor_player[1] = 551
+            # else:
+            #     self.now_jump = False
+            #
+            # if pygame.key.get_pressed()[pygame.K_LEFT] and self.coor_player[0] > 0 and \
+            #         self.field[(self.coor_player[0] - 1) // 24][
+            #             len(self.field[0]) - (self.coor_player[1] + 18) // 24 - 5] == 0 and self.field[col_pl][
+            #     row_pl] == 0:  # ходьба в лево
+            #     win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], size_pl[0], size_pl[1]))
+            #     self.coor_player[0] -= 2
+            #     win.blit(self.player_img, self.coor_player)
+            #     pygame.display.update()
+            # elif pygame.key.get_pressed()[pygame.K_RIGHT] and self.coor_player[0] < 415 and (col_pl == 17 or (
+            #         self.field[col_pl + 1][len(self.field[0]) - (self.coor_player[1] + 18) // 24 - 5] == 0 and
+            #         self.field[col_pl + 1][row_pl] == 0) or (col_pl) * 24 + 8 != self.coor_player[0]):  # ходьба в право
+            #     win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], size_pl[0], size_pl[1]))
+            #     self.coor_player[0] += 2
+            #     win.blit(self.player_img, self.coor_player)
+            #     pygame.display.update()
+
+            if len(self.now_blocks) < 1:
+                self.new_generation_field()
+
+            if not self.now_animation:  # обрабатываем анимацию падения блоков в отдельном потоке
+                self.now_animation = True
+                thr_fall = threading.Thread(target=self.fall_blocks, args=(), name="fall_block")
+                thr_fall.start()
+
+    def move(self):
+        """передвижение игрока"""
+        # self.speed = randint(1, 2)
+        win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], self.size_pl[0], self.size_pl[1]))   # стираем
+        # print('x', self.coor_player[0], 'y', self.coor_player[1])
+        # top = len(self.field[0]) - self.coor_player[1] // 24 - 5
+        # bottom = len(self.field[0]) - self.coor_player[1] // 24 - 5
+        right = self.coor_player[0]+self.size_pl[0] # "x" правой части персонажа
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:  # ходьба в право
+            if self.coor_player[0]+self.speed >= self.field_size[0]-self.size_pl[0]: # стенка справа
+                self.coor_player[0] = self.field_size[0]-self.size_pl[0]
+            elif (self.field[(right+self.speed)//self.size_block][len(self.field[0])-self.coor_player[1]//self.size_block-5] not in self.permeable_blocks) or self.field[(right+self.speed)//self.size_block][len(self.field[0])-(self.coor_player[1]+self.size_pl[1])//self.size_block-5] not in self.permeable_blocks:       # блок справа снизу и справа сверху, если есть, то передвигаемся вплотную к нему
+                while (self.field[(right+1)//self.size_block][len(self.field[0])-self.coor_player[1]//self.size_block-5] in self.permeable_blocks) and self.field[(right+1)//self.size_block][len(self.field[0])-(self.coor_player[1]+self.size_pl[1])//self.size_block-5] in self.permeable_blocks:
+                    self.coor_player[0]+=1
+            else:       # стенки и блока справа нет
+                self.coor_player[0] += self.speed
+
+        if pygame.key.get_pressed()[pygame.K_LEFT]:  # ходьба в лево
+            if self.coor_player[0]-self.speed < 0: # стенка слева
+                self.coor_player[0] = 0
+            elif self.field[(self.coor_player[0]-self.speed)//self.size_block][len(self.field[0])-self.coor_player[1]//self.size_block-5] not in self.permeable_blocks or self.field[(self.coor_player[0]-self.speed)//self.size_block][len(self.field[0])-(self.coor_player[1]+self.size_pl[1])//self.size_block-5] not in self.permeable_blocks:       # блок справа снизу и справа сверху,  если есть, то передвигаемся вплотную к нему
+                while (self.field[(self.coor_player[0]-1)//self.size_block][len(self.field[0])-self.coor_player[1]//self.size_block-5] in self.permeable_blocks) and self.field[(self.coor_player[0]-1)//self.size_block][len(self.field[0])-(self.coor_player[1]+self.size_pl[1])//self.size_block-5] in self.permeable_blocks:
+                    self.coor_player[0]-=1
+            else:       # стенки и блока справа нет
+                self.coor_player[0] -= self.speed
+
+        right = self.coor_player[0]+self.size_pl[0] # "x" правой части персонажа
+        bottom = self.coor_player[1]+self.size_pl[1]+int(self.jump_speed**2) # "y" нижней части персонажа + после падения
+        # bottom_1 = self.coor_player[1]+self.size_pl[1]+1
+
+        if not self.isjump:     # падение
+            if (self.field[self.coor_player[0]//self.size_block][len(self.field[0])-bottom//self.size_block-5] in self.permeable_blocks and self.field[right//self.size_block][len(self.field[0])-bottom//self.size_block-5] in self.permeable_blocks) and bottom < self.field_size[1]:    # если снизу нет блока
+                if self.jump_speed < 2:
+                    self.jump_speed += 0.05
+                self.coor_player[1] += int(self.jump_speed ** 2)
+            else:
+                self.isfall = False
+                self.jump_speed = 1
+                self.coor_player[1] += self.size_block - ((self.coor_player[1]+self.size_pl[1]-1)%self.size_block) - 2
+
+        if self.isjump:     # прыжок
+            if self.jump_speed <= 1:
+                self.isjump = False
+                self.isfall = True
+                self.jump_speed = 1
+                # print('высота прыжка - ', self.height_j)
+            else:
+                self.jump_speed -= 0.03
+                self.coor_player[1] -= int(self.jump_speed**2)
+                # self.height_j += int(self.jump_speed**2)
+
+
+        win.blit(self.player_img, self.coor_player)
+        pygame.display.update()
+
+
+    def setting(self):
+        """настройки игры"""
         self.blocks_old = [[[1, 1], [1, 1]], [[1, 1], [1], [1]], [[1], [1, 1, 1]], [[1, 1, 1], [0, 0, 1]],
                            [[0, 1], [0, 1], [1, 1]], [[1, 1, 1], [0, 1]], [[1], [1, 1], [1]], [[0, 1], [1, 1, 1]],
                            [[0, 1], [1, 1], [0, 1]], [[1], [1], [1], [1]], [[1, 1, 1, 1]], [[1, 1], [0, 1], [0, 1]],
@@ -23,93 +193,17 @@ class tetge():
                        [[0, 1, 1], [1, 1, 0]],  # зигзаг
                        [[1, 1, 0], [0, 1, 1]]]  # зигзаг другой
 
-        self.max_h = 0  # значение максимальной достигнутой высоты персонажем
-        self.field = []  # поле 24 на 18
-        self.now_blocks = []  # блоки в падении
-        self.now_animation = False  # обозначает, что сейчас не происходит прорисовка падения блоков
-        self.now_jump = False
-        height_jump = 55
-        max_jump = 55
-        size_pl = [17, 20]  # размер персонажа в пикселях
-        pygame.font.init()
-        self.stop = False
+        self.speed = 2  # скорость передвижения игрока
+        self.size_pl = [17, 20]  # размер персонажа в пикселях
+        self.size_block = 24    # размер блока
+        self.speed_game = 10   # скорость игры
+        self.permeable_blocks = [0]  # блоки, через которые игрок может проходить (воздух и в будущем бонусы)
 
-        self.coor_player = [192, 552]
-        self.player_img = pygame.image.load('img/918.png')  # выведим игрока
-        self.player_img = pygame.transform.scale(self.player_img, (size_pl[0], size_pl[1]))  # подгоняем размеры
+        self.field_size = [24 * 18, 24 * 24]  # размер поля в пикселях
+        self.coor_player = [192, self.field_size[1]-self.size_pl[1]-2]   # начальные координаты игрока
+        self.player_img = pygame.image.load('img/player.png')      # выведим игрока
+        self.player_img = pygame.transform.scale(self.player_img, (self.size_pl[0], self.size_pl[1]))  # подгоняем размеры персонажа
 
-        for i in range(18):
-            self.field.append([0] * 28)  # одновременно видно будет только 24 клетки (вверх) и 18 клеток в ширину
-        self.height = [0] * 18  # высота каждого столбца
-
-        while 1:
-            col_pl = self.coor_player[0] // 24  # X персонажа
-            row_pl = len(self.field[0]) - self.coor_player[1] // 24 - 5  # Y персонажа
-            pygame.time.delay(5)
-            self.score()
-
-            win.blit(self.player_img, self.coor_player)
-            pygame.display.update()
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                if event.type == pygame.KEYDOWN:
-                    # if event.key == pygame.K_DOWN:
-                    #     self.max_h += 1
-                    #     print(self.max_h)
-                    if event.key == pygame.K_UP and not self.now_jump:  # прыжок
-                        self.now_jump = True
-                        height_jump = 0
-                    if event.key == pygame.K_SPACE:  # ставим синий блок
-                        # win.blit(pygame.image.load('img/cube_blue.png'), (col_pl * 24, (23 - row_pl) * 24))
-                        # print(col_pl, row_pl)
-                        # pygame.display.update()
-                        print("высота сейчас", self.max_h)
-            if self.field[(self.coor_player[0] + 1) // 24][row_pl] != 0 or self.field[(self.coor_player[0] + 15) // 24][
-                row_pl] != 0:  # если достигает блока сверху, то сразу опускается вниз
-                height_jump = max_jump
-            if self.now_jump and height_jump < max_jump and self.field[col_pl][row_pl] == 0 and \
-                    self.field[(self.coor_player[0] + size_pl[0] - 2) // 24][row_pl] == 0:  # взлет прыжка
-                win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], size_pl[0], size_pl[1]))
-                self.coor_player[1] -= 1
-                win.blit(self.player_img, self.coor_player)
-                pygame.display.update()
-                height_jump += 1
-            elif self.coor_player[1] != 552 and (row_pl != 0 and self.field[col_pl][row_pl - 1] == 0 and
-                                                 self.field[(self.coor_player[0] + size_pl[0] - 2) // 24][
-                                                     row_pl - 1] == 0):  # падение
-                # self.now_jump = True
-                win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], size_pl[0], size_pl[1]))
-                self.coor_player[1] += 1
-                win.blit(self.player_img, self.coor_player)
-                pygame.display.update()
-            else:
-                self.now_jump = False
-
-            if pygame.key.get_pressed()[pygame.K_LEFT] and self.coor_player[0] != 0 and \
-                    self.field[(self.coor_player[0] - 1) // 24][
-                        len(self.field[0]) - (self.coor_player[1] + 18) // 24 - 5] == 0 and self.field[col_pl][
-                row_pl] == 0:  # ходьба в лево
-                win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], size_pl[0], size_pl[1]))
-                self.coor_player[0] -= 1
-                win.blit(self.player_img, self.coor_player)
-                pygame.display.update()
-            elif pygame.key.get_pressed()[pygame.K_RIGHT] and self.coor_player[0] != 415 and (col_pl == 17 or (
-                    self.field[col_pl + 1][len(self.field[0]) - (self.coor_player[1] + 18) // 24 - 5] == 0 and
-                    self.field[col_pl + 1][row_pl] == 0) or (col_pl) * 24 + 8 != self.coor_player[0]):  # ходьба в право
-                win.fill((0, 0, 0), (self.coor_player[0], self.coor_player[1], size_pl[0], size_pl[1]))
-                self.coor_player[0] += 1
-                win.blit(self.player_img, self.coor_player)
-                pygame.display.update()
-
-            if len(self.now_blocks) < 1:
-                self.new_generation_field()
-
-            if not self.now_animation:  # обрабатываем анимацию падения блоков в отдельном потоке
-                self.now_animation = True
-                thr_fall = threading.Thread(target=self.fall_blocks, args=(), name="fall_block")
-                thr_fall.start()
 
     def score(self):
         my_font = pygame.font.SysFont('Comic Sans MS', 30)
@@ -119,15 +213,15 @@ class tetge():
 
     def update_win(self):
         """при смене уровня карты, перерисовывает все окно"""
-        color_block = pygame.image.load('img/cube_red.png')  # блок 24 на 24 пикселя
+        red_block = pygame.image.load('img/cube_red.png')  # блок 24 на 24 пикселя
         win.fill((0, 0, 0))
         y_win = 24
         for y in range(len(self.field[0]) - 28, len(self.field[0]) - 4):
             y_win -= 1
             for x in range(len(self.field)):
                 if self.field[x][y] == 1:
-                    win.blit(color_block, (x * 24, y_win * 24))
-                    pygame.display.update()
+                    win.blit(red_block, (x * 24, y_win * 24))
+        pygame.display.update()
                 # elif self.field[x][y] == 2:
                 #     win.blit(pygame.image.load('img/cube_blue.png'), (x * 24, y_win * 24))
                 #     pygame.display.update()
@@ -164,7 +258,6 @@ class tetge():
         for rt in range(4):
             block = self.rotate_block(self.now_block, rt)
             max_x = 18 - len(block)
-
             for x in range(0, max_x + 1):
                 now_space = 0
                 maximum_h = self.height[x] + 23
@@ -175,14 +268,14 @@ class tetge():
                         stop_h = self.height[x + i] - b.count(0)  # высота, на которой остановиться блок после падении
                 for i in range(0, len(block)):
                     now_space += (stop_h + block[i].count(0) - self.height[x + i])
-                if now_space < x_info[1] or (now_space == x_info[1] and stop_h < x_info[2]):
+                if now_space < x_info[1] or (now_space == x_info[1] and stop_h < x_info[2]) or (now_space == x_info[1] and stop_h == x_info[2] and randint(0, 1)):
                     x_info[2] = stop_h
                     x_info[1] = now_space
                     x_info[0] = x
                     now_block = block
         for i in range(len(now_block)):
             self.height[x_info[0] + i] = x_info[2] + len(now_block[i])  # пересчитываем высоту после падения блока
-        print(x_info)
+        # print(x_info)
         self.now_blocks.append([now_block, x_info[2], x_info[0], len(self.field[0]) - 5, 0])
 
     def rotate_block(self, block, rt):
@@ -237,7 +330,7 @@ class tetge():
                         if ((self.coor_player[0] + 1) // 24 == place + x or (
                                 self.coor_player[0] + 15) // 24 == place + x) and len(self.field[0]) - (
                                 self.coor_player[1] + 20) // 24 - 5 == y + i:
-                            print('вы проиграли! Ваша максимальная высота:', self.max_h)
+                            print('вы проиграли!')
                             self.score()
                             pygame.quit()
 
